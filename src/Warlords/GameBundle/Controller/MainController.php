e70f9abb765bec04de214117bab08f1932137dee
lilmocca <lilmocca@gmail.com>
2012-11-02 15:53:12 -0700

    initial commit

---
+++ src/Warlords/GameBundle/Controller/MainController.php
@@ -0,0 +1,14 @@
+<?php
+// src/Warlords/GameBundle/Controller/MainController.php
+
+namespace Warlords\GameBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('WarlordsGameBundle:Page:index.html.twig');
+    }
+}
---
+++ src/Warlords/GameBundle/WarlordsGameBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Warlords\GameBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WarlordsGameBundle extends Bundle
+{
+}
