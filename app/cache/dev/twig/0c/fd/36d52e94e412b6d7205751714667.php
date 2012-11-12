7c83647e43990ea00719ee0928d2fd53e69079f4
andy <andy@andy-VirtualBox.(none)>
2012-11-12 10:18:04 -0800

    shows self stats when viewing other profiles

--- app/cache/dev/twig/0c/fd/36d52e94e412b6d7205751714667.php
+++ app/cache/dev/twig/0c/fd/36d52e94e412b6d7205751714667.php
@@ -12,7 +12,6 @@ class __TwigTemplate_0cfd36d52e94e412b6d7205751714667 extends Twig_Template
         $this->blocks = array(
             'title' => array($this, 'block_title'),
             'stylesheets' => array($this, 'block_stylesheets'),
-            'blog_title' => array($this, 'block_blog_title'),
             'navigation' => array($this, 'block_navigation'),
             'login' => array($this, 'block_login'),
             'sidebar' => array($this, 'block_sidebar'),
@@ -49,17 +48,12 @@ class __TwigTemplate_0cfd36d52e94e412b6d7205751714667 extends Twig_Template
 
         <div id=\"container\"><div id=\"innercontainer\">
             <header id=\"header\">
-                <hgroup>
-                    <h2>";
-        // line 20
-        $this->displayBlock('blog_title', $context, $blocks);
-        echo "</h2>
-                </hgroup>
+               
 \t\t\t\t<div class=\"top\">
                     ";
-        // line 23
+        // line 21
         $this->displayBlock('navigation', $context, $blocks);
-        // line 44
+        // line 41
         echo "                </div>
             </header>
 
@@ -67,36 +61,36 @@ class __TwigTemplate_0cfd36d52e94e412b6d7205751714667 extends Twig_Template
 \t\t\t\t<aside class=\"sidebar\">
     \t\t\t\t<div class=\"login\">
 \t\t\t\t    ";
-        // line 50
+        // line 47
         $this->displayBlock('login', $context, $blocks);
-        // line 51
+        // line 48
         echo "\t\t\t\t    </div>
 \t\t            ";
-        // line 52
+        // line 49
         $this->displayBlock('sidebar', $context, $blocks);
-        // line 53
+        // line 50
         echo "\t\t        </aside>
 
 \t\t        <section class=\"main-col\">
 \t\t            ";
-        // line 56
+        // line 53
         $this->displayBlock('body', $context, $blocks);
-        // line 57
+        // line 54
         echo "\t\t        </section>
 \t\t\t</div>
 
             <div id=\"footer\">
                 ";
-        // line 61
+        // line 58
         $this->displayBlock('footer', $context, $blocks);
-        // line 64
+        // line 61
         echo "            </div>
         </div></div>
 
         ";
-        // line 67
+        // line 64
         $this->displayBlock('javascripts', $context, $blocks);
-        // line 68
+        // line 65
         echo "    </body>
 </html>
 ";
@@ -118,68 +112,61 @@ class __TwigTemplate_0cfd36d52e94e412b6d7205751714667 extends Twig_Template
         ";
     }
 
-    // line 20
-    public function block_blog_title($context, array $blocks = array())
-    {
-        echo "<a href=\"#\">Warlords: The Final Allegiance</a>";
-    }
-
-    // line 23
+    // line 21
     public function block_navigation($context, array $blocks = array())
     {
-        // line 24
+        // line 22
         echo "                        <nav>
                             <ul class=\"navigation\">
                                 <li><a href=\"#\">Home</a></li>
                                 <li><a href=\"#\">About</a></li>
-\t\t\t\t<li><a href=\"#\">Guide</a></li>
-
-\t\t\t\t<li><a href=\"#\">Ranking</a>
-\t\t\t\t\t<ul>
-           \t\t\t\t\t<li><a href=”#”>Wealth</a></li>
-            \t\t\t\t\t<li><a href=”#”>Fame</a></li>
-        \t\t\t\t</ul>\t\t\t\t\t\t\t\t\t
-\t\t\t\t</li>
-
-\t\t\t\t<li><a href=\"";
-        // line 37
+\t\t\t\t\t\t\t\t<li><a href=\"#\">Guide</a></li>
+\t\t\t\t\t\t\t\t<li><a href=\"#\">Ranking</a>
+\t\t\t\t\t\t\t\t\t<ul>
+           \t\t\t\t \t\t\t\t<li><a href=”#”>Wealth</a></li>
+\t\t\t\t\t\t\t\t\t\t<li><a href=”#”>Test</a></li>
+            \t\t\t\t\t\t\t<li><a href=”#”>Fame</a></li>
+        \t\t\t\t\t\t\t</ul>\t\t\t\t\t\t\t\t\t
+\t\t\t\t\t\t\t\t</li>
+\t\t\t\t\t\t\t\t<li><a href=\"";
+        // line 34
         echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("WarlordsGameBundle_profile"), "html", null, true);
         echo "\">Profile</a></li>
-\t\t\t\t<li><a href=\"";
-        // line 38
+\t\t\t\t\t\t\t\t<li><a href=\"";
+        // line 35
         echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("WarlordsGameBundle_player"), "html", null, true);
         echo "\">Player</a></li>
-\t\t\t\t<li><a href=\"#\">Guild</a></li>
+\t\t\t\t\t\t\t\t<li><a href=\"#\">Guild</a></li>
                                 <li><a href=\"#\">Contact</a></li>
                             </ul>
                         </nav>
                     ";
     }
 
-    // line 50
+    // line 47
     public function block_login($context, array $blocks = array())
     {
     }
 
-    // line 52
+    // line 49
     public function block_sidebar($context, array $blocks = array())
     {
     }
 
-    // line 56
+    // line 53
     public function block_body($context, array $blocks = array())
     {
     }
 
-    // line 61
+    // line 58
     public function block_footer($context, array $blocks = array())
     {
-        // line 62
+        // line 59
         echo "                    Warlords: The Final Allegiance - © 2012 WeWeb
                 ";
     }
 
-    // line 67
+    // line 64
     public function block_javascripts($context, array $blocks = array())
     {
     }
@@ -196,6 +183,6 @@ class __TwigTemplate_0cfd36d52e94e412b6d7205751714667 extends Twig_Template
 
     public function getDebugInfo()
     {
-        return array (  183 => 67,  178 => 62,  175 => 61,  170 => 56,  165 => 52,  160 => 50,  150 => 38,  146 => 37,  131 => 24,  128 => 23,  122 => 20,  115 => 11,  112 => 10,  106 => 6,  100 => 68,  98 => 67,  93 => 64,  91 => 61,  85 => 57,  83 => 56,  78 => 53,  76 => 52,  73 => 51,  71 => 50,  63 => 44,  61 => 23,  55 => 20,  44 => 13,  42 => 10,  35 => 6,  31 => 5,  28 => 1,);
+        return array (  170 => 64,  165 => 59,  162 => 58,  157 => 53,  152 => 49,  147 => 47,  137 => 35,  133 => 34,  119 => 22,  116 => 21,  109 => 11,  106 => 10,  100 => 6,  94 => 65,  92 => 64,  87 => 61,  85 => 58,  79 => 54,  77 => 53,  72 => 50,  70 => 49,  67 => 48,  65 => 47,  57 => 41,  55 => 21,  43 => 13,  41 => 10,  34 => 6,  27 => 1,);
     }
 }
--- src/Warlords/GameBundle/Controller/ProfileController.php
+++ src/Warlords/GameBundle/Controller/ProfileController.php
@@ -32,23 +32,34 @@ class ProfileController extends Controller{
     }
     
     public function profile_getAction($target_id){
-        $id=$target_id;
+        $usr = $this->get('security.context')->getToken()->getUser();
+        $id = $usr->getID();
         
         $em = $this->getDoctrine()->getEntityManager();
 
-        $playerstats = $em->getRepository('WarlordsGameBundle:PlayerStats')->findOneByUser($id);
+        $playerstats = $em->getRepository('WarlordsGameBundle:PlayerStats')->findOneByUser($target_id);
+        $selfstats = $em->getRepository('WarlordsGameBundle:PlayerStats')->findOneByUser($id);
 
 
         if (!$playerstats) {
         	throw $this->createNotFoundException('Unable to find player.');
     	}
+    	if (!$selfstats) {
+        	throw $this->createNotFoundException('Unable to find player.');
+    	}
     	
-    	$soldiers = $playerstats->getInfantry();
-    	$knights = $playerstats->getKnights();
-    	$calvary = $playerstats->getCalvary();
+    	$soldiers = $selfstats->getInfantry();
+    	$knights = $selfstats->getKnights();
+    	$calvary = $selfstats->getCalvary();
+    	
+    	$attk = $soldiers + $knights*2 + $calvary*4;
+    	$defn = $soldiers + $knights*3 + $calvary*3;
         
         return $this->render('WarlordsGameBundle:Page:profile_get.html.twig', array(
-                                'playerstats' => $playerstats));
+                                'playerstats' => $playerstats,
+                                'selfstats' => $selfstats,
+                                'attack' => $attk,
+                                'defense' => $defn));
     }
 }
 ?>
