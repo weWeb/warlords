53ac385a72ca4fa7a68c535982146632b54b5864
lilmocca <lilmocca@gmail.com>
2012-11-15 00:37:29 -0800

    reversed some changes

--- src/Warlords/GameBundle/Entity/User.php
+++ src/Warlords/GameBundle/Entity/User.php
@@ -21,291 +21,12 @@ class User extends BaseUser
     /**
      * @var boolean $isActive
      */
-    protected $isActive;
+    private $isActive;
     
  
- 
-    /**
-     * @var string
-     */
-    protected $username;
+    private $alliesWithMe;
 
-    /**
-     * @var string
-     */
-    protected $usernameCanonical;
-
-    /**
-     * @var string
-     */
-    protected $salt;
-
-    /**
-     * @var string
-     */
-    protected $password;
-
-    /**
-     * @var string
-     */
-    protected $email;
-
-    /**
-     * @var string
-     */
-    protected $emailCanonical;
-
-    /**
-     * @var boolean
-     */
-    protected $enabled;
-
-    /**
-     * @var \DateTime
-     */
-    protected $lastLogin;
-
-    /**
-     * @var string
-     */
-    protected $confirmationToken;
-
-    /**
-     * @var \DateTime
-     */
-    protected $passwordRequestedAt;
-
-    /**
-     * @var boolean
-     */
-    protected $locked;
-
-    /**
-     * @var boolean
-     */
-    protected $expired;
-
-    /**
-     * @var \DateTime
-     */
-    protected $expiresAt;
-
-    /**
-     * @var array
-     */
-    protected $roles;
-
-    /**
-     * @var boolean
-     */
-    protected $credentialsExpired;
-
-    /**
-     * @var \DateTime
-     */
-    protected $credentialsExpireAt;
-
-    /**
-     * @var \Warlords\GameBundle\Entity\PlayerStats
-     */
-    protected $stats;
-
-    /**
-     * @var \Doctrine\Common\Collections\Collection
-     */
-    protected $alliesWithMe;
-
-    /**
-     * @var \Doctrine\Common\Collections\Collection
-     */
-    protected $myAllies;
-
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        $this->salt = base_convert(sha1(uniqid(mt_rand(), true)), 16, 36);
-        $this->enabled = false;
-        $this->locked = false;
-        $this->expired = false;
-        $this->roles = array();
-        $this->credentialsExpired = false;
-        $this->alliesWithMe = new \Doctrine\Common\Collections\ArrayCollection();
-        $this->myAllies = new \Doctrine\Common\Collections\ArrayCollection();
-        
-    }
-    
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set username
-     *
-     * @param string $username
-     * @return User
-     */
-    public function setUsername($username)
-    {
-        $this->username = $username;
-    
-        return $this;
-    }
-
-    /**
-     * Get username
-     *
-     * @return string 
-     */
-    public function getUsername()
-    {
-        return $this->username;
-    }
-
-    /**
-     * Set usernameCanonical
-     *
-     * @param string $usernameCanonical
-     * @return User
-     */
-    public function setUsernameCanonical($usernameCanonical)
-    {
-        $this->usernameCanonical = $usernameCanonical;
-    
-        return $this;
-    }
-
-    /**
-     * Get usernameCanonical
-     *
-     * @return string 
-     */
-    public function getUsernameCanonical()
-    {
-        return $this->usernameCanonical;
-    }
-
-    /**
-     * Set salt
-     *
-     * @param string $salt
-     * @return User
-     */
-    public function setSalt($salt)
-    {
-        $this->salt = $salt;
-    
-        return $this;
-    }
-
-    /**
-     * Get salt
-     *
-     * @return string 
-     */
-    public function getSalt()
-    {
-        return $this->salt;
-    }
-
-    /**
-     * Set password
-     *
-     * @param string $password
-     * @return User
-     */
-    public function setPassword($password)
-    {
-        $this->password = $password;
-    
-        return $this;
-    }
-
-    /**
-     * Get password
-     *
-     * @return string 
-     */
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    /**
-     * Set email
-     *
-     * @param string $email
-     * @return User
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-    
-        return $this;
-    }
-
-    /**
-     * Get email
-     *
-     * @return string 
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    /**
-     * Set emailCanonical
-     *
-     * @param string $emailCanonical
-     * @return User
-     */
-    public function setEmailCanonical($emailCanonical)
-    {
-        $this->emailCanonical = $emailCanonical;
-    
-        return $this;
-    }
-
-    /**
-     * Get emailCanonical
-     *
-     * @return string 
-     */
-    public function getEmailCanonical()
-    {
-        return $this->emailCanonical;
-    }
-
-    /**
-     * Set enabled
-     *
-     * @param boolean $enabled
-     * @return User
-     */
-    public function setEnabled($enabled)
-    {
-        $this->enabled = $enabled;
-    
-        return $this;
-    }
-
-    /**
-     * Get enabled
-     *
-     * @return boolean 
-     */
-    public function getEnabled()
-    {
-        return $this->enabled;
-    }
+    private $myAllies;
 
     /**
      * Set isActive
@@ -331,239 +52,20 @@ class User extends BaseUser
     }
 
     /**
-     * Set lastLogin
-     *
-     * @param \DateTime $lastLogin
-     * @return User
+     * Constructor
      */
-    public function setLastLogin(\DateTime $lastLogin)
+    public function __construct()
     {
-        $this->lastLogin = $lastLogin;
+        parent::__construct();
+        $this->isActive = false;
+        $this->alliesWithMe = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->myAllies = new \Doctrine\Common\Collections\ArrayCollection();
+    }
     
-        return $this;
-    }
-
-    /**
-     * Get lastLogin
-     *
-     * @return \DateTime 
-     */
-    public function getLastLogin()
-    {
-        return $this->lastLogin;
-    }
-
-    /**
-     * Set confirmationToken
-     *
-     * @param string $confirmationToken
-     * @return User
-     */
-    public function setConfirmationToken($confirmationToken)
-    {
-        $this->confirmationToken = $confirmationToken;
-    
-        return $this;
-    }
-
-    /**
-     * Get confirmationToken
-     *
-     * @return string 
-     */
-    public function getConfirmationToken()
-    {
-        return $this->confirmationToken;
-    }
-
-    /**
-     * Set passwordRequestedAt
-     *
-     * @param \DateTime $passwordRequestedAt
-     * @return User
-     */
-    public function setPasswordRequestedAt(\DateTime $passwordRequestedAt = null)
-    {
-        $this->passwordRequestedAt = $passwordRequestedAt;
-    
-        return $this;
-    }
-
-    /**
-     * Get passwordRequestedAt
-     *
-     * @return \DateTime 
-     */
-    public function getPasswordRequestedAt()
-    {
-        return $this->passwordRequestedAt;
-    }
-
-    /**
-     * Set locked
-     *
-     * @param boolean $locked
-     * @return User
-     */
-    public function setLocked($locked)
-    {
-        $this->locked = $locked;
-    
-        return $this;
-    }
-
-    /**
-     * Get locked
-     *
-     * @return boolean 
-     */
-    public function getLocked()
-    {
-        return $this->locked;
-    }
-
-    /**
-     * Set expired
-     *
-     * @param boolean $expired
-     * @return User
-     */
-    public function setExpired($expired)
-    {
-        $this->expired = $expired;
-    
-        return $this;
-    }
-
-    /**
-     * Get expired
-     *
-     * @return boolean 
-     */
-    public function getExpired()
-    {
-        return $this->expired;
-    }
-
-    /**
-     * Set expiresAt
-     *
-     * @param \DateTime $expiresAt
-     * @return User
-     */
-    public function setExpiresAt(\DateTime $expiresAt)
-    {
-        $this->expiresAt = $expiresAt;
-    
-        return $this;
-    }
-
-    /**
-     * Get expiresAt
-     *
-     * @return \DateTime 
-     */
-    public function getExpiresAt()
-    {
-        return $this->expiresAt;
-    }
-
-    /**
-     * Set roles
-     *
-     * @param array $roles
-     * @return User
-     */
-    public function setRoles(array $roles)
-    {
-        $this->roles = $roles;
-    
-        return $this;
-    }
-
-    /**
-     * Get roles
-     *
-     * @return array 
-     */
-    public function getRoles()
-    {
-        return $this->roles;
-    }
-
-    /**
-     * Set credentialsExpired
-     *
-     * @param boolean $credentialsExpired
-     * @return User
-     */
-    public function setCredentialsExpired($credentialsExpired)
-    {
-        $this->credentialsExpired = $credentialsExpired;
-    
-        return $this;
-    }
-
-    /**
-     * Get credentialsExpired
-     *
-     * @return boolean 
-     */
-    public function getCredentialsExpired()
-    {
-        return $this->credentialsExpired;
-    }
-
-    /**
-     * Set credentialsExpireAt
-     *
-     * @param \DateTime $credentialsExpireAt
-     * @return User
-     */
-    public function setCredentialsExpireAt(\DateTime $credentialsExpireAt)
-    {
-        $this->credentialsExpireAt = $credentialsExpireAt;
-    
-        return $this;
-    }
-
-    /**
-     * Get credentialsExpireAt
-     *
-     * @return \DateTime 
-     */
-    public function getCredentialsExpireAt()
-    {
-        return $this->credentialsExpireAt;
-    }
-
-    /**
-     * Set stats
-     *
-     * @param \Warlords\GameBundle\Entity\PlayerStats $stats
-     * @return User
-     */
-    public function setStats(\Warlords\GameBundle\Entity\PlayerStats $stats = null)
-    {
-        $this->stats = $stats;
-    
-        return $this;
-    }
-
-    /**
-     * Get stats
-     *
-     * @return \Warlords\GameBundle\Entity\PlayerStats 
-     */
-    public function getStats()
-    {
-        return $this->stats;
-    }
-
     /**
      * Add alliesWithMe
      *
-     * @param \Warlords\GameBundle\Entity\User $alliesWithMe
+     * @param Warlords\GameBundle\Entity\User $alliesWithMe
      * @return User
      */
     public function addAlliesWithMe(\Warlords\GameBundle\Entity\User $alliesWithMe)
@@ -576,7 +78,7 @@ class User extends BaseUser
     /**
      * Remove alliesWithMe
      *
-     * @param \Warlords\GameBundle\Entity\User $alliesWithMe
+     * @param Warlords\GameBundle\Entity\User $alliesWithMe
      */
     public function removeAlliesWithMe(\Warlords\GameBundle\Entity\User $alliesWithMe)
     {
@@ -586,7 +88,7 @@ class User extends BaseUser
     /**
      * Get alliesWithMe
      *
-     * @return \Doctrine\Common\Collections\Collection 
+     * @return Doctrine\Common\Collections\Collection 
      */
     public function getAlliesWithMe()
     {
@@ -596,10 +98,10 @@ class User extends BaseUser
     /**
      * Add myAllies
      *
-     * @param \Warlords\GameBundle\Entity\User $myAllies
+     * @param Warlords\GameBundle\Entity\User $myAllies
      * @return User
      */
-    public function addMyAllie(\Warlords\GameBundle\Entity\User $myAllies)
+    public function addMyAllies(\Warlords\GameBundle\Entity\User $myAllies)
     {
         $this->myAllies[] = $myAllies;
     
@@ -609,9 +111,9 @@ class User extends BaseUser
     /**
      * Remove myAllies
      *
-     * @param \Warlords\GameBundle\Entity\User $myAllies
+     * @param Warlords\GameBundle\Entity\User $myAllies
      */
-    public function removeMyAllie(\Warlords\GameBundle\Entity\User $myAllies)
+    public function removeMyAllies(\Warlords\GameBundle\Entity\User $myAllies)
     {
         $this->myAllies->removeElement($myAllies);
     }
@@ -619,10 +121,40 @@ class User extends BaseUser
     /**
      * Get myAllies
      *
-     * @return \Doctrine\Common\Collections\Collection 
+     * @return Doctrine\Common\Collections\Collection 
      */
     public function getMyAllies()
     {
         return $this->myAllies;
     }
+
+    /**
+     * @var \Warlords\GameBundle\Entity\Playerstats
+     */
+    private $stats;
+
+
+    /**
+     * Set stats
+     *
+     * @param \Warlords\GameBundle\Entity\Playerstats $stats
+     * @return User
+     */
+    public function setStats(\Warlords\GameBundle\Entity\Playerstats $stats = null)
+    {
+        $this->stats = $stats;
+    
+        return $this;
+    }
+
+    /**
+     * Get stats
+     *
+     * @return \Warlords\GameBundle\Entity\Playerstats 
+     */
+    public function getStats()
+    {
+        return $this->stats;
+    }
 }
+
