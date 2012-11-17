572248fb0b1e59c24dbbec02a8f7211f0c6e8729
andy <andy@andy-VirtualBox.(none)>
2012-11-17 11:28:47 -0800

    Old cronjob is wrong. Fixed

--- install_cronjob.sh
+++ install_cronjob.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-newline="* 1 * * * /usr/bin/php /var/www/warlords/app/console WarlordsGameBundle:ResourceUpdate 2>~/crontab.log"
+newline="0 * * * * /usr/bin/php /var/www/warlords/app/console WarlordsGameBundle:ResourceUpdate 2>~/crontab.log"
 (crontab -l; echo "$newline") | crontab
