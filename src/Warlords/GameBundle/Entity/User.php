12c9f5ce2fc7c5b6ffeff330475f2df40d8e27ad
yfw1 <yfw1@sfu.ca>
2012-12-02 07:11:32 -0800

    Basic ally page and listing

--- src/Warlords/GameBundle/Entity/User.php
+++ src/Warlords/GameBundle/Entity/User.php
@@ -7,41 +7,58 @@ use FOS\UserBundle\Model\User as BaseUser;
 use FOS\MessageBundle\Model\ParticipantInterface;
 
 /**
- * @ORM\Entity
- * @ORM\Table(name="user")
+ * User
  */
 class User extends BaseUser implements ParticipantInterface
 {
     /**
-     * @ORM\Id
-     * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue(strategy="AUTO")
+     * @var integer
      */
     protected $id;
 
     /**
-     * @var boolean $isActive
+     * @var boolean
      */
     private $isActive;
-    
 
-    private $alliesWithMe;
-
-    private $myAllies;
-    
     /**
-     * @var \DateTime $lastAccess
+     * @var \DateTime
      */
     private $lastAccess;
 
 
-    
+    /**
+     * @var \Warlords\GameBundle\Entity\PlayerStats
+     */
+    private $stats;
 
     /**
      * @var \Warlords\GameBundle\Entity\Guild
      */
     private $guild;
+
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $alliesWithMe;
+
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $myAllies;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+    	parent::__construct();
+        $this->isActive = false;
+        $this->alliesWithMe = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->myAllies = new \Doctrine\Common\Collections\ArrayCollection();
+    }
     
+
     /**
      * Set isActive
      *
@@ -66,20 +83,79 @@ class User extends BaseUser implements ParticipantInterface
     }
 
     /**
-     * Constructor
+     * Set lastAccess
+     *
+     * @param \DateTime $lastAccess
+     * @return User
      */
-    public function __construct()
+    public function setLastAccess($lastAccess)
     {
-        parent::__construct();
-        $this->isActive = false;
-        $this->alliesWithMe = new \Doctrine\Common\Collections\ArrayCollection();
-        $this->myAllies = new \Doctrine\Common\Collections\ArrayCollection();
-    }
+        $this->lastAccess = $lastAccess;
     
+        return $this;
+    }
+
+    /**
+     * Get lastAccess
+     *
+     * @return \DateTime 
+     */
+    public function getLastAccess()
+    {
+        return $this->lastAccess;
+    }
+
+
+    /**
+     * Set stats
+     *
+     * @param \Warlords\GameBundle\Entity\PlayerStats $stats
+     * @return User
+     */
+    public function setStats(\Warlords\GameBundle\Entity\PlayerStats $stats = null)
+    {
+        $this->stats = $stats;
+    
+        return $this;
+    }
+
+    /**
+     * Get stats
+     *
+     * @return \Warlords\GameBundle\Entity\PlayerStats 
+     */
+    public function getStats()
+    {
+        return $this->stats;
+    }
+
+    /**
+     * Set guild
+     *
+     * @param \Warlords\GameBundle\Entity\Guild $guild
+     * @return User
+     */
+    public function setGuild(\Warlords\GameBundle\Entity\Guild $guild = null)
+    {
+        $this->guild = $guild;
+    
+        return $this;
+    }
+
+    /**
+     * Get guild
+     *
+     * @return \Warlords\GameBundle\Entity\Guild 
+     */
+    public function getGuild()
+    {
+        return $this->guild;
+    }
+
     /**
      * Add alliesWithMe
      *
-     * @param Warlords\GameBundle\Entity\User $alliesWithMe
+     * @param \Warlords\GameBundle\Entity\User $alliesWithMe
      * @return User
      */
     public function addAlliesWithMe(\Warlords\GameBundle\Entity\User $alliesWithMe)
@@ -92,7 +168,7 @@ class User extends BaseUser implements ParticipantInterface
     /**
      * Remove alliesWithMe
      *
-     * @param Warlords\GameBundle\Entity\User $alliesWithMe
+     * @param \Warlords\GameBundle\Entity\User $alliesWithMe
      */
     public function removeAlliesWithMe(\Warlords\GameBundle\Entity\User $alliesWithMe)
     {
@@ -102,7 +178,7 @@ class User extends BaseUser implements ParticipantInterface
     /**
      * Get alliesWithMe
      *
-     * @return Doctrine\Common\Collections\Collection 
+     * @return \Doctrine\Common\Collections\Collection 
      */
     public function getAlliesWithMe()
     {
@@ -112,10 +188,10 @@ class User extends BaseUser implements ParticipantInterface
     /**
      * Add myAllies
      *
-     * @param Warlords\GameBundle\Entity\User $myAllies
+     * @param \Warlords\GameBundle\Entity\User $myAllies
      * @return User
      */
-    public function addMyAllies(\Warlords\GameBundle\Entity\User $myAllies)
+    public function addMyAllie(\Warlords\GameBundle\Entity\User $myAllies)
     {
         $this->myAllies[] = $myAllies;
     
@@ -125,9 +201,9 @@ class User extends BaseUser implements ParticipantInterface
     /**
      * Remove myAllies
      *
-     * @param Warlords\GameBundle\Entity\User $myAllies
+     * @param \Warlords\GameBundle\Entity\User $myAllies
      */
-    public function removeMyAllies(\Warlords\GameBundle\Entity\User $myAllies)
+    public function removeMyAllie(\Warlords\GameBundle\Entity\User $myAllies)
     {
         $this->myAllies->removeElement($myAllies);
     }
@@ -135,7 +211,7 @@ class User extends BaseUser implements ParticipantInterface
     /**
      * Get myAllies
      *
-     * @return Doctrine\Common\Collections\Collection 
+     * @return \Doctrine\Common\Collections\Collection 
      */
     public function getMyAllies()
     {
@@ -153,80 +229,4 @@ class User extends BaseUser implements ParticipantInterface
         $user->alliesWithMe[] = $this;
         $this->myAllies[] = $user;
     }
-
-    /**
-     * @var \Warlords\GameBundle\Entity\Playerstats
-     */
-    private $stats;
-
-
-    /**
-     * Set stats
-     *
-     * @param \Warlords\GameBundle\Entity\Playerstats $stats
-     * @return User
-     */
-    public function setStats(\Warlords\GameBundle\Entity\Playerstats $stats = null)
-    {
-        $this->stats = $stats;
-    
-        return $this;
-    }
-
-    /**
-     * Get stats
-     *
-     * @return \Warlords\GameBundle\Entity\Playerstats 
-     */
-    public function getStats()
-    {
-        return $this->stats;
-    }
-    
-    /**
-    * Set guild
-    *
-    * @param \Warlords\GameBundle\Entity\Guild $guild
-    * @return User
-    */
-    public function setGuild(\Warlords\GameBundle\Entity\Guild $guild = null)
-    {
-        $this->guild = $guild;
-    
-        return $this;
-    }
-
-    /**
-    * Get guild
-    *
-    * @return \Warlords\GameBundle\Entity\Guild 
-    */
-    public function getGuild()
-    {
-        return $this->guild;
-    }
-    
-    /** Set the last access time.
-     *
-     * @return \DateTime
-     */
-    public function setLastAccess(\DateTime $time)
-    {
-        $this->lastAccess = $time;
-
-        return $this;
-    }
-    
-    
-    /**
-     * Get the last access time.
-     *
-     * @return \DateTime
-     */
-    public function getLastAccess()
-    {
-        return $this->lastAccess;
- 
-    }   
 }
-
